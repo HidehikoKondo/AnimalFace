633ebb9085bbb1193ccac02f72a56c000e87d99b
Hidehiko Kondo <dev@udonko.net>
2017-09-21 17:51:44 +0900

    カメラ撮影

--- iOS/AnimalFace/AnimalFace/CameraViewController.swift
+++ iOS/AnimalFace/AnimalFace/CameraViewController.swift
@@ -15,25 +15,73 @@ import AVFoundation
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var cameraView: UIView!
-    
+    //カメラセッション
+    var captureSession: AVCaptureSession!
+    //デバイス
+    var cameraDevices: AVCaptureDevice!
+    //画像のアウトプット
+    var imageOutput: AVCaptureStillImageOutput!
+
+    @IBOutlet weak var thumbnailView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.viewGradient()
 
-        //カメラ表示
-        cameraView.layer.addSublayer(cameraLayer)
-        let output = AVCaptureVideoDataOutput()
-        output.setSampleBufferDelegate(self as? AVCaptureVideoDataOutputSampleBufferDelegate, queue: DispatchQueue(label: "queue"))
-        captureSession.addOutput(output)
+        //セッションの作成
+        captureSession = AVCaptureSession()
+
+        //デバイス一覧の取得
+        let devices = AVCaptureDevice.devices()
+
+        //バックカメラをcameraDevicesに格納
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                cameraDevices = device as! AVCaptureDevice
+            }
+        }
+
+        //バックカメラからVideoInputを取得
+        let videoInput: AVCaptureInput!
+        do {
+            videoInput = try AVCaptureDeviceInput.init(device: cameraDevices)
+        } catch {
+            videoInput = nil
+        }
+
+        //セッションに追加
+        captureSession.addInput(videoInput)
+
+        //出力先を生成
+        imageOutput = AVCaptureStillImageOutput()
+
+        //セッションに追加
+        captureSession.addOutput(imageOutput)
+
+        //画像を表示するレイヤーを生成
+        let captureVideoLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
+        captureVideoLayer.frame = self.cameraView.bounds
+        captureVideoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+
+        //Viewに追加
+        self.cameraView.layer.addSublayer(captureVideoLayer)
+
+        //セッション開始
         captureSession.startRunning()
+
+        //カメラ表示
+//        cameraView.layer.addSublayer(cameraLayer)
+//        let output = AVCaptureVideoDataOutput()
+//        output.setSampleBufferDelegate(self as? AVCaptureVideoDataOutputSampleBufferDelegate, queue: DispatchQueue(label: "queue"))
+//        captureSession.addOutput(output)
+//        captureSession.startRunning()
 
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        cameraLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        cameraLayer.frame = CGRect(x: 0, y: 0, width: cameraView.bounds.width, height: cameraView.bounds.width )
+//        cameraLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+//        cameraLayer.frame = CGRect(x: 0, y: 0, width: cameraView.bounds.width, height: cameraView.bounds.width )
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,19 +92,35 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
 
 
     //カメラキャプチャー
-    private lazy var captureSession: AVCaptureSession = {
-        let session = AVCaptureSession()
-        session.sessionPreset = AVCaptureSession.Preset.photo
-        guard let backCamera = AVCaptureDevice.default(for: .video),
-            let input = try? AVCaptureDeviceInput(device: backCamera) else {
-                return session
+//    private lazy var captureSession: AVCaptureSession = {
+//        let session = AVCaptureSession()
+//        session.sessionPreset = AVCaptureSession.Preset.photo
+//        guard let backCamera = AVCaptureDevice.default(for: .video),
+//            let input = try? AVCaptureDeviceInput(device: backCamera) else {
+//                return session
+//        }
+//        session.addInput(input)
+//        return session
+//    }()
+//    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+
+
+    @IBAction func takePhoto(_ sender: Any) {
+        //ビデオ出力に接続
+        let captureVideoConnection = imageOutput.connection(with: AVMediaType.video)
+        
+        //接続から画像を取得
+        self.imageOutput.captureStillImageAsynchronously(from: captureVideoConnection!) { (imageDataBuffer, error) -> Void in
+            //取得したImageのDataBufferをJPEGを変換
+            let capturedImageData: NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer!) as! NSData
+            //JPEGからUIImageを作成
+            let Image: UIImage = UIImage(data: capturedImageData as Data)!
+            //アルバムに追加
+            //UIImageWriteToSavedPhotosAlbum(Image, self, nil, nil)
+
+            self.thumbnailView.image = Image
         }
-        session.addInput(input)
-        return session
-    }()
-    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-
-
+    }
 
 
     @IBAction func back(_ sender: Any) {
