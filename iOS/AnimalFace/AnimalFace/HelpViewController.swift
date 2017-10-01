a0100f1cc4c21e6af9891e8f654510b93caea2ff
UDONKONET <udonkonet@UDONKONETnoMacBook.local>
2017-10-01 17:54:27 +0900

    ヘルプ追加

--- iOS/AnimalFace/AnimalFace/HelpViewController.swift
+++ iOS/AnimalFace/AnimalFace/HelpViewController.swift
@@ -18,6 +18,8 @@ class HelpViewController: UIViewController, GADBannerViewDelegate   {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.viewGradient()
+
     }
     
     override func viewDidLayoutSubviews() {
@@ -30,6 +32,27 @@ class HelpViewController: UIViewController, GADBannerViewDelegate   {
         // Dispose of any resources that can be recreated.
     }
     
+    func viewGradient(){
+        //グラデーションの開始色
+        let topColor = UIColor(red:0.76, green:0.94, blue:0.98, alpha:1)
+        //グラデーションの開始色
+        let bottomColor = UIColor(red:0.78, green:0.97, blue:0.81, alpha:1)
+        
+        //グラデーションの色を配列で管理
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        
+        //グラデーションレイヤーを作成
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        
+        //グラデーションの色をレイヤーに割り当てる
+        gradientLayer.colors = gradientColors
+        //グラデーションレイヤーをスクリーンサイズにする
+        gradientLayer.frame = self.view.bounds
+        
+        //グラデーションレイヤーをビューの一番下に配置
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
 
     /*
     // MARK: - Navigation
