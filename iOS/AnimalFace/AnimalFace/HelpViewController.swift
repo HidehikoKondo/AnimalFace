8350f4409102daf0f19513e21468851888855512
Hidehiko Kondo <dev@udonko.net>
2017-10-27 18:45:16 +0900

    ヘルプ画面のAutoLayout設定

--- iOS/AnimalFace/AnimalFace/HelpViewController.swift
+++ iOS/AnimalFace/AnimalFace/HelpViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import GoogleMobileAds
 
 class HelpViewController: UIViewController, GADBannerViewDelegate   {
-    
+
     @IBOutlet weak var adView: UIView!
     var bannerView: GADBannerView!
 
@@ -80,10 +80,10 @@ class HelpViewController: UIViewController, GADBannerViewDelegate   {
         
         //ビュー位置調整
         print("safeview-----------------", self.view.safeAreaInsets)
-        self.adView.frame = CGRect(x: self.adView.frame.origin.x,
-                                   y: self.view.frame.height - self.adView.frame.height -  self.view.safeAreaInsets.bottom,
-                                   width: self.adView.frame.width,
-                                   height: self.adView.frame.height)
+//        self.adView.frame = CGRect(x: self.adView.frame.origin.x,
+//                                   y: self.view.frame.height - self.adView.frame.height -  self.view.safeAreaInsets.bottom,
+//                                   width: self.adView.frame.width,
+//                                   height: self.adView.frame.height)
     }
     
     /// Tells the delegate an ad request loaded an ad.
